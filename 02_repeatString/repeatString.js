30169f3fdebabe8ab32a083e1b4079030f91347f
Ryan Jewik <Ryanjewik@gmail.com>
2023-06-04 15:07:22 -0700

    adding exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(stringInput, numRepeats) {
+    let stringOutput = stringInput;
+    for (i = 0; i < numRepeats -1; i++){
+        stringOutput = stringOutput + stringInput;
+    }
+    return stringOutput;
 };
 
 // Do not edit below this line
