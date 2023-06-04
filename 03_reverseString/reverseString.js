95ae9eda34342b7337912f714324a9187cc191bc
Ryan Jewik <Ryanjewik@gmail.com>
2023-06-04 16:45:03 -0700

    adding exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let reversedString = "";
+    for (i = 0; i < inputString.length; i++){
+        reversedString += inputString.charAt(inputString.length - i - 1);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
