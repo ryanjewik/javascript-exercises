494567bd095bc3088f6518a64da482ef32866abe
Ryan Jewik <Ryanjewik@gmail.com>
2023-06-04 19:33:05 -0700

    adding exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(catalogArr) {
+    var newArr = [];
+    for (i = 0; i < catalogArr.length; i++){
+        newArr[i] = catalogArr[i]['title'];
+    }
+    return newArr;
 };
 
 // Do not edit below this line
