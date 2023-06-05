f2dd2fc4240ea2dff1301e492c3a35eecf5ac4c4
Ryan Jewik <Ryanjewik@gmail.com>
2023-06-04 18:39:03 -0700

    adding exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  //let celsius = 0;
+  celsius = ((fahrenheit -32) / (9/5));
+  if (celsius % 1 === 0){
+    celsius = celsius.toFixed();
+  }
+  else{
+    celsius = celsius.toFixed(1);
+  }
+  return Number(celsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  //let fahrenheit = 0;
+  fahrenheit = ((celsius * (9/5)) + 32);
+  if (fahrenheit % 1 === 0){
+    fahrenheit = fahrenheit.toFixed();
+  }
+  else{
+    fahrenheit = fahrenheit.toFixed(1);
+  }
+  return Number(fahrenheit);
 };
 
 // Do not edit below this line
