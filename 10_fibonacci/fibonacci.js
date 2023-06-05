8d2fa40ed817963f23699f29cae30c629d3c1e54
Ryan Jewik <Ryanjewik@gmail.com>
2023-06-04 19:19:58 -0700

    adding exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(fiboNum) {
+    fiboNum = Number(fiboNum);
+    if (fiboNum < 0){
+        return "OOPS";
+    }else if(fiboNum <=2){
+        return 1;
+    }
+    
+    firstNum = 1;
+    secondNum = 1;
+    for (i = 3; i < fiboNum; i++){
+        if (i % 2=== 0){
+            firstNum = firstNum + secondNum;
+        }else if (i % 2=== 1){
+            secondNum = firstNum + secondNum;
+        }
+        outputNum = firstNum + secondNum;
+    }
+    return outputNum;
 };
 
 // Do not edit below this line
