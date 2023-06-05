f7e1b7dc6c2713e86f646b075abe3aaababe8f15
Ryan Jewik <Ryanjewik@gmail.com>
2023-06-04 18:11:36 -0700

    adding exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    if (numTwo < numOne){ //swaps if numOne is greater
+        let tempNum = numTwo;
+        numTwo = numOne;
+        numOne = tempNum;
+    }
+    let sumOutput = 0;
+    if (numOne > 0 && numTwo > 0 && typeof numOne === "number" && typeof numTwo ==="number"){
+        for (i = numOne; i <= numTwo; i++){
+            sumOutput = sumOutput + i;
+        }
+        return sumOutput;
+    }
+    else{
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
