0e583b46f57e16737e84169d4f2868f070250abd
Ryan Jewik <Ryanjewik@gmail.com>
2023-06-04 19:07:12 -0700

    adding exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    var punctuation = /[\.,?!]/g;
+    word = word.replace(punctuation, "");
+    word = word.replace(/\s+/g, "");
+    word = word.toLowerCase();
+    
+    let reversedWord ="";
+    for (i = 0; i < word.length; i++){
+        reversedWord = reversedWord + word[word.length - 1 - i];
+    }
+    //console.log(reversedWord);
+    if(word === reversedWord){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
