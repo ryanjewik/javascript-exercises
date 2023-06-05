527539569d945a579f1162a98fb96d2ec84c384e
Ryan Jewik <Ryanjewik@gmail.com>
2023-06-04 17:36:10 -0700

    Adding exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,34 @@
 const removeFromArray = function() {
+    /*
+    if (typeof removedItems === "string"){
+        return removedItems;
+    }
+    */
+   if (arguments.length > 2){
+        for (i = 1; i <= arguments.length -1; i++){
+
+            let removeIndex = arguments[0].indexOf(arguments[i])
+            if (removeIndex != -1){
+                if(typeof arguments[0][removeIndex] === typeof arguments[i]){
+                    arguments[0].splice(removeIndex, 1);
+                }
+            }
+            //console.log(i + " test" + '\n');
+        }
+        
+    }
+    else{
+        let removeIndex = arguments[0].indexOf(arguments[1])
+        if (removeIndex != -1){
+            if(typeof arguments[0][removeIndex] === typeof arguments[1]){
+                arguments[0].splice(removeIndex, 1);
+            }
+        }
+    }
+    //console.log(arguments[0]);
+    return arguments[0];
+    
+    //return arguments.length;
 
 };
 
